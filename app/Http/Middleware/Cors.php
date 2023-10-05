a2e92cac9c4f74598a74dfc12900fca547c4d6a9
Wesley Matos <wamk-9@hotmail.com>
2023-10-04 23:08:28 -0300

    Inserted CORS middleware to work API use on others domains

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class Cors extends Middleware
+{
+    public function handle(Request $request, Closure $next): Response
+	{
+		return $next($request)
+		->header('Access-Control-Allow-Origin', "*")
+		->header('Access-Control-Allow-Methods', "PUT, POST, DELETE, GET, OPTIONS")
+		->header('Access-Control-Allow-Headers', "Accept, Authorization, Content-Type");
+	} 
+}
